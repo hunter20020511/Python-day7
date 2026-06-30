1b8fe77eca6308e19b32f0950f82d96a1ba32623
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-30 19:18:47 +0530

    Added necessary comments

--- Day7_Basics1_Warm_up.py
+++ Day7_Basics1_Warm_up.py
@@ -1,6 +1,6 @@
 text = "  Hello !, I am  welcoming you  to Python.  "
 
-#print(text.strip())
-#print(text.lower())
-print(text.replace("", ","))
-#print(text.split())
+print(text.strip()) #Removes whitespaces
+print(text.lower()) #Converts all characters to lowe case
+print(text.replace("", ",")) #Replaces all occurencies of a substring, can replace words too
+print(text.split()) #splits string into list of words, by first converting them into list
