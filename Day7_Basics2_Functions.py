164e47ca8a85d2aa4b8ed6de4dbedeab5bc705a1
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-07-03 22:50:44 +0530

    Made function for clear text

--- Day7_Basics2_Functions.py
+++ Day7_Basics2_Functions.py
@@ -1,9 +1,39 @@
 def clean_text(text):
     return text.strip().lower()
-def replace(text2):
-    text2= text2.replace("bad", "Good")
-    text2= text2.replace("nice", "Nachiket")
-    return text2
-    
 
-print(replace(" you ARE bad guy,  nice   da "))
+
+def replace_words(text):
+    replacements = {
+        "bad": "good",
+        "nice": "nachiket"
+    }
+
+    for word, replacement in replacements.items():
+        text = text.replace(word, replacement)
+
+    return text
+
+
+def remove_punctuation(text):
+    for ch in ",.!?():;'":
+        text = text.replace(ch, "")
+    return text
+
+
+#Pipeline
+def process_text(text):
+    pipeline = [clean_text, replace_words, remove_punctuation]
+
+    for func in pipeline:
+        text = func(text)
+
+    return text
+
+
+#take input frm user
+input_text = input("Type your text for cleaning: ")
+
+#Process the input took from user
+output = process_text(input_text)
+
+print("Final Output:", output)
